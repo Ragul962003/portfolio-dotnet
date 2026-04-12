cd1764d387ff98815dd008dd5fcf8d50df6930d7
Ragul962003 <ragulvincent09@gmail.com>
2026-04-12 20:46:46 +0530

    fix resume download route

--- Controllers/ResumeController.cs
+++ Controllers/ResumeController.cs
@@ -7,22 +7,25 @@ using Microsoft.AspNetCore.Mvc;
         [ApiController]
         public class ResumeController : ControllerBase
         {
-            [HttpGet("download")]
-            public IActionResult DownloadResume()
+        [HttpGet("download")]
+        public IActionResult DownloadResume()
+        {
+            var filePath = Path.Combine(
+                Directory.GetCurrentDirectory(),
+                "wwwroot",
+                "resumes",
+                "Ragul_Fullstack_Developer_resume.pdf"
+            );
+
+            if (!System.IO.File.Exists(filePath))
             {
-                var filePath = Path.Combine(
-                    Directory.GetCurrentDirectory(),
-                    "C:\\Users\\LENOVO\\Documents\\resumes\\Ragul_resume.pdf"
-                );
-
-                if (!System.IO.File.Exists(filePath))
-                {
-                    return NotFound("Resume not found");
-                }
-
-                var fileBytes = System.IO.File.ReadAllBytes(filePath);
-
-                return File(fileBytes, "application/pdf", "Ragul_Resume.pdf");
+                return NotFound("Resume not found");
             }
-        }
+
+            var fileBytes = System.IO.File.ReadAllBytes(filePath);
+
+            return File(fileBytes, "application/pdf", "Ragul_Fullstack_Developer_resume.pdf");
+        
+    }
+}
     }
